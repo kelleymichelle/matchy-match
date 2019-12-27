0623181ee76b48346b073198463cb75affc7065c
Kelley Chaplain <kmchaplain@gmail.com>
2019-12-26 22:47:51 -0600

    changed db for backend

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MatchyMatch
+module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -33,5 +33,13 @@ module MatchyMatch
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+    
   end
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,4 @@
-console.log("Luke I am your father")
+const BACKEND_URL = 'localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
